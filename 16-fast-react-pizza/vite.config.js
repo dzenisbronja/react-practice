21f200f8d4a5070f907beb821f2931df8ccf68de
bronja9 <otraditi@gmail.com>
2024-02-09 10:09:18 +0100

    progress

--- 16-fast-react-pizza/vite.config.js
+++ 16-fast-react-pizza/vite.config.js
@@ -1,8 +1,30 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import eslint from "vite-plugin-eslint";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import eslintPlugin from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      // Konfiguracija ESLint-a
+      cache: false, // Onemogućiti kesiranje za sprečavanje problema sa ažuriranjem
+      fix: true, // Popraviti automatski ispravljive greške
+      extensions: [".js", ".jsx"], // Ekstenzije fajlova koje ESLint treba da analizira
+      exclude: ["node_modules", ".git"], // Direktorijumi koje ESLint treba da izuzme iz analize
+      overrideConfig: {
+        rules: {
+          "no-unused-vars": "off", // Isključuje greške za neiskorišćene promenljive
+        },
+      },
+    }),
+  ],
 });
