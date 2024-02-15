296c323e83cafeabe669bee5c0b783a376d286e6
bronja9 <otraditi@gmail.com>
2024-02-15 16:21:44 +0100

    progress

--- 17-the-wild-oasis/vite.config.js
+++ 17-the-wild-oasis/vite.config.js
@@ -1,8 +1,30 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import eslint from "vite-plugin-eslint";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import eslintPlugin from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      fix: true,
+      extensions: [".js", ".jsx"],
+      exclude: ["node_modules", ".git"],
+      overrideConfig: {
+        rules: {
+          "no-unused-vars": "off",
+          "no-constant-condition": "off", // Isključuje pravilo za konstantni uslov
+        },
+      },
+    }),
+  ],
 });
