422b6c93baae4d7d97829c55ccdfb35601750948
bronja9 <otraditi@gmail.com>
2024-02-27 18:24:20 +0100

    progress

--- 17-the-wild-oasis/src/features/authentication/useLogin.js
+++ 17-the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,9 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
@@ -14,7 +15,8 @@ export function useLogin() {
       }),
 
     onSuccess: (user) => {
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (err) => {
