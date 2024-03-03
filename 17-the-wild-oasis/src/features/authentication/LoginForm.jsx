e7f577c0e375bc82c43b46317007cc56814d80dc
bronja9 <otraditi@gmail.com>
2024-03-03 19:54:59 +0100

    progress

--- 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,7 +7,7 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("jonas@example.com");
+  const [email, setEmail] = useState("production@gmail.com");
   const [password, setPassword] = useState("pass0987");
   const { login, isLoading } = useLogin();
 
