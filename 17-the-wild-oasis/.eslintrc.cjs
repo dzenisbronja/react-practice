892ebc0aebb85aba7836e39a9bd683a2b13d8ddd
bronja9 <otraditi@gmail.com>
2024-03-04 12:57:34 +0100

    progress

--- 17-the-wild-oasis/.eslintrc.cjs
+++ 17-the-wild-oasis/.eslintrc.cjs
@@ -13,9 +13,9 @@ module.exports = {
   plugins: ["react-refresh"],
   rules: {
     "react/prop-types": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    // "react-refresh/only-export-components": [
+    // "warn",
+    // { allowConstantExport: true },
+    // ],
   },
 };
